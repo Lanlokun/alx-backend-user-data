06747416d30287367e7a1148a9a6e5b51f7279c0
malik <mlanlokun@gmail.com>
2023-08-04 14:14:42 +0300

    Regex-ing

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,11 @@
+# !/usr/bin/env python3
+"""regex-ing"""
+
+import re
+
+def filter_datum(fields: list, redaction: str, message: str, separator: str) -> str:
+    """returns the log message obfuscated"""
+    for field in fields:
+        message = re.sub(field + "=.*?" + separator,
+                         field + "=" + redaction + separator, message)
+    return message
