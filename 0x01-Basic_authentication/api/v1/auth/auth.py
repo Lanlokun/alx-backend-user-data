5be74647e30fc0da374ea18cbaa788104b064316
malik <mlanlokun@gmail.com>
2023-08-09 03:21:16 +0300

    request validation updates

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -24,10 +24,7 @@ class Auth:
         """
         if request is None:
             return None
-        if 'Authorization' not in request.headers:
-            return None
-        else:
-            return request.headers['Authorization']
+        return request.headers.get('Authorization', None)
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Current user
