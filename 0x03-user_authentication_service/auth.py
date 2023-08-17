d3ea4784d9c2107a7bb62daa200bdb4d128f9085
malik <mlanlokun@gmail.com>
2023-08-17 06:14:08 +0300

    Generate UUIDs

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
@@ -15,6 +16,10 @@ def _hash_password(password: str) -> bytes:
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """ Check if password is valid """
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+
+def _generate_uuid() -> str:
+    """ Generate a UUID """
+    return str(uuid.uuid4())
 
 
 class Auth:
