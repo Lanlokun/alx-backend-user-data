9033e5aba726217a55ac883f46a47ce92a40acfa
malik <mlanlokun@gmail.com>
2023-08-17 04:53:45 +0300

    User model

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""user model"""
+
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    """User class.
+    """
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
+
+    def __init__(self, *args, **kwargs):
+        """Constructor.
+        """
+        super().__init__(*args, **kwargs)
+    
+    def __str__(self):
+        """String representation.
+        """
+        return "<User {}>".format(self.email)
